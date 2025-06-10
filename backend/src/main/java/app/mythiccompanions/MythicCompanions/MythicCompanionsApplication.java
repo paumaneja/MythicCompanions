38c09c5738efebd50847b40ba9bb09ced76b3fb6
paumaneja <paumaneja@gmail.com>
2025-06-10 17:38:10 +0200

    Initial commit: Set up monorepo structure and initialize Spring Boot backend

---
+++ backend/src/main/java/app/mythiccompanions/MythicCompanions/MythicCompanionsApplication.java
@@ -0,0 +1,13 @@
+package app.mythiccompanions.MythicCompanions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MythicCompanionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MythicCompanionsApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/app/mythiccompanions/MythicCompanions/MythicCompanionsApplicationTests.java
@@ -0,0 +1,13 @@
+package app.mythiccompanions.MythicCompanions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MythicCompanionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
